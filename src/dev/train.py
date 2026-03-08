ccf93903ac2f13a50443463ca10ca0046eb8f3b4
BrunoSantome <bruno.santomeantolin@gmail.com>
2026-03-08 10:40:57 +0000

    init repository files

---
+++ src/dev/train.py
@@ -0,0 +1,2 @@
+# – Train.py - running python train.py should start a train loop, and should also log results. Parameters
+# can be passed to this file to select nr_epochs and other relevant parameters.
